a0ae236b5d257e32f8d0dac3ed4d150a66add310
qinpeng <qinpeng@didichuxing.com>
2016-11-15 14:36:50 +0800

    add img

--- memery_pool/test.c
+++ memery_pool/test.c
@@ -9,10 +9,9 @@ struct user{
 
 int main(void)
 {
-    sleep(30);
     pool_t *p;
 
-    p = create_pool(2*1024);
+    p = create_pool(200*1024*1024);
 
     struct user *u, *u1;
     int i = 0;
@@ -22,17 +21,10 @@ int main(void)
         u->id = 123;
         u->name = "pangudashu";
 
-        //pool_pfree(p, u);
         u1 = pool_alloc(p, 10240);
     }
-    sleep(30);
 
     destroy_pool(p);
-
-    printf("ddd pagesize:%d\n", MAX_ALLOC_FROM_POOL);
-    //printf("user.id:%d name:%s\n", u->id, u->name);
-
-    sleep(40);
 
     return 0;
 }
